ec75799fd7270d0b3f7f14fc1bab2015cc6d77c5
Ryan Silva <ryandotsilva@gmail.com>
2019-04-26 14:02:32 -0400

    Update MovieDisplay to use props

--- client/src/components/display/MovieDisplay.js
+++ client/src/components/display/MovieDisplay.js
@@ -5,12 +5,16 @@ class MovieDisplay extends Component {
 		return (
 			<div className="movie-border-box">
 				<div className="movie-image-container">
-					<img src="" alt="" className="move-image" />
+					<img src="" alt="" className="movie-image" />
 				</div>
 				<div className="movie-info">
-					<h1 className="movie-title">TEST</h1>
-					<p className="movie-release-date">Date</p>
-					<p className="movie-overview">Overview</p>
+					<h1 className="movie-title">{this.props.movie.title}</h1>
+					<p className="movie-release-date">
+						{this.props.movie.date}
+					</p>
+					<p className="movie-overview">
+						{this.props.movie.overview}
+					</p>
 				</div>
 			</div>
 		);
