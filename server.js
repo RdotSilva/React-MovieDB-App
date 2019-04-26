ea0bc1e1705c1158c2f5ddd3fb3e68b072fd3372
Ryan Silva <ryandotsilva@gmail.com>
2019-04-26 10:42:12 -0400

    Add and use routes in server

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
+const movie = require('./routes/api/movie');
+const actor = require('./routes/api/actor');
+const grossing = require('./routes/api/grossing');
+
 const app = express();
 
 app.get('/', (req, res) => res.send('Test'));
+
+// Use Routes
+app.use('/api/movie', movie);
+app.use('/api/actor', actor);
+app.use('/api/grossing', grossing);
 
 const port = process.env.PORT || 5000;
 
