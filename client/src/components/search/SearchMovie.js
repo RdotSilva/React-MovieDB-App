9e412dcff769fb2fb6e438074aa91410f4860c67
Ryan Silva <ryandotsilva@gmail.com>
2019-04-26 15:51:15 -0400

    Add searchForMovies method and pass in as search

--- client/src/components/search/SearchMovie.js
+++ client/src/components/search/SearchMovie.js
@@ -2,31 +2,26 @@ import React, { Component } from 'react';
 import Search from './Search';
 import axios from 'axios';
 
+const apiKey = '53d84fb2d3fffb38f67498a4522c2728';
 const baseURL = 'https://api.themoviedb.org/3/';
 
 class SearchMovie extends Component {
-	constructor(props) {
-		super(props);
-	}
 	state = {
 		movies: [],
-		completed: false,
-		searchValue: this.props.searchValue
+		completed: false
 	};
 
-	componentDidMount() {
+	searchForMovies = searchValue => {
 		axios
 			.get(
-				`${baseURL}search/movie?api_key=${apiKey}&language=en-US&query=${
-					this.state.searchValue
-				}&page=1&include_adult=false`
+				`${baseURL}search/movie?api_key=${apiKey}&language=en-US&query=${searchValue}&page=1&include_adult=false`
 			)
 			.then(res => this.setState({ movies: res.data, completed: true }));
 		console.log(this.props);
-	}
+	};
 
 	render() {
-		return <Search />;
+		return <Search search={this.searchForMovies} />;
 	}
 }
 
