cd3fe72001fc503e3a8ce4b15cd4a90266a536bb
Ryan Silva <ryandotsilva@gmail.com>
2019-04-26 15:35:05 -0400

    Add axios request to SearchMovie component

--- client/src/components/search/SearchMovie.js
+++ client/src/components/search/SearchMovie.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import Search from './Search';
+import axios from 'axios';
+
+const baseURL = 'https://api.themoviedb.org/3/';
 
 class SearchMovie extends Component {
+	constructor(props) {
+		super(props);
+	}
+	state = {
+		movies: [],
+		completed: false,
+		searchValue: this.props.searchValue
+	};
+
+	componentDidMount() {
+		axios
+			.get(
+				`${baseURL}search/movie?api_key=${apiKey}&language=en-US&query=${
+					this.state.searchValue
+				}&page=1&include_adult=false`
+			)
+			.then(res => this.setState({ movies: res.data, completed: true }));
+		console.log(this.props);
+	}
+
 	render() {
 		return <Search />;
 	}
