5c42301bcf699afcf06648e335b23007db1fa8a8
Ryan Silva <RyandotSilva@gmail.com>
2019-05-01 17:17:04 -0400

    Update styles to use module.css file

--- client/src/components/display/MovieDisplay.js
+++ client/src/components/display/MovieDisplay.js
@@ -3,29 +3,34 @@ import styles from './MovieDisplay.module.css';
 
 class MovieDisplay extends Component {
 	render() {
+		// Use deconstructor to pull movie from props
+		const { movie } = this.props;
+		let baseImgUrl = 'http://image.tmdb.org/t/p/w185/';
 		let imageSource;
+
+		// Check if movie image is available, if not use default image
 		if (
-			`${baseImgUrl}${e.poster_path}` ===
+			`${baseImgUrl}${movie.poster_path}` ===
 			'http://image.tmdb.org/t/p/w185/null'
 		) {
 			imageSource = '/noimage.jpg';
 		} else {
-			imageSource = `${baseImgUrl}${e.poster_path}`;
+			imageSource = `${baseImgUrl}${movie.poster_path}`;
 		}
 
 		return (
-			<div className="movie-border-box">
-				<div className="movie-image-container">
-					<img src="" alt="" className="movie-image" />
+			<div className={styles['movie-border-box']}>
+				<div className={styles['movie-image-container']}>
+					<img
+						src={imageSource}
+						alt=""
+						className={styles['movie-image']}
+					/>
 				</div>
-				<div className="movie-info">
-					<h1 className="movie-title">{this.props.movie.title}</h1>
-					<p className="movie-release-date">
-						{this.props.movie.date}
-					</p>
-					<p className="movie-overview">
-						{this.props.movie.overview}
-					</p>
+				<div className={styles['movie-info']}>
+					<h1 className={styles['movie-title']}>{movie.title}</h1>
+					<p className={styles['movie-release-date']}>{movie.date}</p>
+					<p className={styles['movie-overview']}>{movie.overview}</p>
 				</div>
 			</div>
 		);
