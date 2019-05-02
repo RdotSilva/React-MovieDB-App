0af4ea36e205e3fecd5931b1a8baf2635e3903ba
Ryan Silva <RyandotSilva@gmail.com>
2019-05-02 16:40:30 -0400

    Add getMovieCredits method

--- client/src/components/search/SearchActor.js
+++ client/src/components/search/SearchActor.js
@@ -9,7 +9,9 @@ const baseURL = 'https://api.themoviedb.org/3/';
 class SearchActor extends Component {
 	state = {
 		actorId: [],
-		actorDetails: []
+		actorDetails: [],
+		allMovieCredits: [],
+		eightMovieCredits: []
 	};
 
 	searchPerson = async searchValue => {
@@ -23,18 +25,35 @@ class SearchActor extends Component {
 	};
 
 	getPersonDetails = async () => {
-		await axios(
-			`${baseURL}person/${
-				this.state.actorId
-			}?api_key=${apiKey}&language=en-US`
-		).then(res => this.setState({ actorDetails: res.data }));
+		await axios
+			.get(
+				`${baseURL}person/${
+					this.state.actorId
+				}?api_key=${apiKey}&language=en-US`
+			)
+			.then(res => this.setState({ actorDetails: res.data }));
 		console.log(this.state);
 	};
 
 	showPersonDetails = searchValue => {
-		this.searchPerson(searchValue).then(id => {
-			this.getPersonDetails(id);
-		});
+		this.searchPerson(searchValue)
+			.then(id => {
+				this.getPersonDetails(id);
+			})
+			.then(() => this.getMovieCredits());
+	};
+
+	getMovieCredits = async () => {
+		await axios
+			.get(
+				`${baseURL}person/${
+					this.state.actorId
+				}/movie_credits?api_key=${apiKey}&language=en-US`
+			)
+			.then(movies => {
+				this.setState({ allMovieCredits: movies.data });
+			});
+		console.log(this.state);
 	};
 
 	render() {
