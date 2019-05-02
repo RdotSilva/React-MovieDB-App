18db20e87b455778a0b4f5987a86118f73a4e2d4
Ryan Silva <RyandotSilva@gmail.com>
2019-05-02 15:45:05 -0400

    Add searchPerson method

--- client/src/components/search/SearchActor.js
+++ client/src/components/search/SearchActor.js
@@ -7,6 +7,16 @@ import apiKey from '../../config_keys';
 const baseURL = 'https://api.themoviedb.org/3/';
 
 class SearchActor extends Component {
+	searchPerson = async searchValue => {
+		// Search for person. This will give access to ID of person.
+		const response = await axios
+			.get(
+				`${baseURL}search/person?api_key=${apiKey}&query=${searchValue}`
+			)
+			.then(res => this.setState({ actorId: res.data.results }));
+		console.log(this.state);
+	};
+
 	render() {
 		return <div />;
 	}
