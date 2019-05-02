8fa506c12526b54d719102c55c341f81e02241fc
Ryan Silva <RyandotSilva@gmail.com>
2019-05-02 15:49:17 -0400

    Add SearchActor route into main app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import SearchMovie from './components/search/SearchMovie';
+import SearchActor from './components/search/SearchActor';
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
 					<Navbar />
 					<Route exact path="/" component={Landing} />
 					<Route exact path="/searchmovie" component={SearchMovie} />
+					<Route exact path="/searchactor" component={SearchActor} />
 				</div>
 			</Router>
 		);
