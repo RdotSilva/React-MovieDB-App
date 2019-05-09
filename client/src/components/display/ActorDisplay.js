f5c738526cc3ce2cf95eb949402dda9c8f103aff
Ryan Silva <RyandotSilva@gmail.com>
2019-05-08 20:00:08 -0400

    Update jsx to use classNames

--- client/src/components/display/ActorDisplay.js
+++ client/src/components/display/ActorDisplay.js
@@ -20,24 +20,32 @@ class ActorDisplay extends Component {
 		}
 
 		return (
-            <div className="actor-border-box">
-                <div className="actor-image-container">
-                        <img className="actor-image" src="${imageSource}">
-                </div>
-                <div className="actor-info">
-                    <h1 className="actor-title">{actor.name}</h1>
-                    <p className="actor-birthday">Born on: {/*this.formatDate*/(actor.birthday)}</p>
-                    <a href="https://www.imdb.com/name/${actor.imdb_id}/">IMDB</a>
-                </div>
-                <div id="actor-movie-credits">
-                    <ul id="actor-movie-credits-list">
-                    </ul>   
-                </div>
-                <div className="actor-bio">
-                    <p className="actor-bio-text">{actor.biography}</p>
-                </div>
-            </div>
-        );
+			<div className={styles['actor-border-box']}>
+				<div className={styles['actor-image-container']}>
+					<img
+						className={styles['actor-image']}
+						src={this.imageSource}
+					/>
+				</div>
+				<div className={styles['actor-info']}>
+					<h1 className={styles['actor-title']}>{actor.name}</h1>
+					<p className={styles['actor-birthday']}>
+						Born on: {actor.birthday}
+					</p>
+					<a href="https://www.imdb.com/name/${actor.imdb_id}/">
+						IMDB
+					</a>
+				</div>
+				<div className={styles['actor-movie-credits']}>
+					<ul className={styles['actor-movie-credits-list']} />
+				</div>
+				<div className={styles['actor-bio']}>
+					<p className={styles['actor-bio-text']}>
+						{actor.biography}
+					</p>
+				</div>
+			</div>
+		);
 	}
 }
 
