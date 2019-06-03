e05372ad2bb0a33821899f7cb3c93104a4803c77
Ryan Silva <RyandotSilva@gmail.com>
2019-06-02 20:41:23 -0400

    Update ActorDisplay component

--- client/src/components/display/ActorDisplay.js
+++ client/src/components/display/ActorDisplay.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import styles from './ActorDisplay.module.css';
-import noImageFound from '../../../src/img/noimage.jpg';
+import React, { Component } from "react";
+import styles from "./ActorDisplay.module.css";
+import noImageFound from "../../../src/img/noimage.jpg";
 
 class ActorDisplay extends Component {
 	render() {
-		const actor = this.props.actor;
-		let baseImgUrl = 'http://image.tmdb.org/t/p/w185/';
+		const { actor } = this.props;
+		let baseImgUrl = "http://image.tmdb.org/t/p/w185/";
 		let imageSource;
 
 		// Check if actor image is available, if not use default image
 		if (
 			`${baseImgUrl}${actor.profile_path}` ===
-			'http://image.tmdb.org/t/p/w185/null'
+			"http://image.tmdb.org/t/p/w185/null"
 		) {
 			imageSource = noImageFound;
 		} else {
@@ -19,16 +19,16 @@ class ActorDisplay extends Component {
 		}
 
 		return (
-			<div className={styles['actor-border-box']}>
-				<div className={styles['actor-image-container']}>
+			<div className={styles["actor-border-box"]}>
+				<div className={styles["actor-image-container"]}>
 					<img
-						className={styles['actor-image']}
+						className={styles["actor-image"]}
 						src={this.imageSource}
 					/>
 				</div>
-				<div className={styles['actor-info']}>
-					<h1 className={styles['actor-title']}>{actor.name}</h1>
-					<p className={styles['actor-birthday']}>{actor.birthday}</p>
+				<div className={styles["actor-info"]}>
+					<h1 className={styles["actor-title"]}>{actor.name}</h1>
+					<p className={styles["actor-birthday"]}>{actor.birthday}</p>
 					{actor.imdb_id !== undefined ? (
 						<a href={`https://www.imdb.com/name/${actor.imdb_id}/`}>
 							IMDB
@@ -37,11 +37,11 @@ class ActorDisplay extends Component {
 						<a />
 					)}
 				</div>
-				<div className={styles['actor-movie-credits']}>
-					<ul className={styles['actor-movie-credits-list']} />
+				<div className={styles["actor-movie-credits"]}>
+					<ul className={styles["actor-movie-credits-list"]} />
 				</div>
-				<div className={styles['actor-bio']}>
-					<p className={styles['actor-bio-text']}>
+				<div className={styles["actor-bio"]}>
+					<p className={styles["actor-bio-text"]}>
 						{actor.biography}
 					</p>
 				</div>
