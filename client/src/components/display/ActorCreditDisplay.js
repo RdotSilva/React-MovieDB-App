887c3cc77a5d472d0f1c77d4c5859e1a8f6ba728
Ryan Silva <RyandotSilva@gmail.com>
2019-06-02 20:47:57 -0400

    Update ActorCreditDisplay

---
+++ client/src/components/display/ActorCreditDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import styles from "./ActorCreditDisplay.module.css";
+
+class ActorCreditDisplay extends Component {
+	render() {
+		const { actor } = this.props;
+		return <div />;
+	}
+}
+
+export default ActorCreditDisplay;
